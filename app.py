6f32ce56c366a56c3a20516d45a4dd937cf70a4e
jordan607 <vishal.thakur607@gmail.com>
2025-09-14 13:58:31 +0530

    port

--- app.py
+++ app.py
@@ -91,4 +91,5 @@ def delete_song(filename):
     return jsonify({'success': False, 'error': 'File not found'}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
